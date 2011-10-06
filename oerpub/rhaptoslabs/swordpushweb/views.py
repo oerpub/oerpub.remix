ffd8cc1001add89991a5e5282ed83a35ebaa6694
Johan Beyers <jbeyers@juizi.com>
2011-10-06 13:31:26 +0200

    Moved views into a flatter hierarchy for now

--- oerpub/rhaptoslabs/swordpushweb/views.py
+++ oerpub/rhaptoslabs/swordpushweb/views.py
@@ -1,7 +1,51 @@
 import oerpub.rhaptoslabs.sword1cnx as swordcnx
 from languages import languages
 
-def UploadView(request):
+def my_view(request):
+    return { 'project':'SwordPush' }
+
+
+def auth_view(request):
+    """
+    Handle authentication (login) requests.
+    """
+    # TODO: check credentials against Connexions and ask for login
+    # again if failed.
+
+    serviceDocument = request.params['serviceDocument']
+
+    # Encode authentication information
+    import base64
+    username = request.params['username']
+    password = request.params['password']
+    digest = base64.b64encode(username + ':' + password)
+
+    # Authenticate to Connexions
+    conn = swordcnx.Connection(serviceDocument,
+                               user_name=username,
+                               user_pass=password,
+                               download_service_document=True)
+
+    # Get available collections from SWORD service document
+    swordCollections = swordcnx.parse_service_document(conn.sd)
+
+
+    return {
+        'serviceDocument': serviceDocument,
+        'credentials': digest,
+        'swordCollections': swordCollections,
+        'url': '',
+        'title': '',
+        'keepTitle': False,
+        'summary': '',
+        'keepSummary': False,
+        'keywords': '',
+        'keepKeywords': True,
+        'languages': languages,
+        'language': 'en',
+    }
+
+def upload_view(request):
     """
     Handle SWORD uploads POSTed from a form.
     """
--- oerpub/rhaptoslabs/swordpushweb/views/MyView.py
+++
@@ -1,3 +0,0 @@
-def MyView(request):
-    return { 'project':'SwordPush' }
-
