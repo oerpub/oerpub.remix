43a821eb1832ec9f1d58e74b7703eceeab4025c4
Johan Beyers <jbeyers@juizi.com>
2011-10-06 13:53:06 +0200

    Some PEP8 treatment

--- oerpub/rhaptoslabs/swordpushweb/views.py
+++ oerpub/rhaptoslabs/swordpushweb/views.py
@@ -1,3 +1,5 @@
+import os
+import base64
 import oerpub.rhaptoslabs.sword1cnx as swordcnx
 from languages import languages
 
@@ -15,7 +17,6 @@ def auth_view(request):
     serviceDocument = request.params['serviceDocument']
 
     # Encode authentication information
-    import base64
     username = request.params['username']
     password = request.params['password']
     digest = base64.b64encode(username + ':' + password)
@@ -53,7 +54,6 @@ def upload_view(request):
     inputs = request.params
 
     # Decode authentication information
-    import base64
     authString = base64.b64decode(inputs['credentials'])
     pos = authString.find(':')
     username = authString[:pos]
@@ -69,19 +69,24 @@ def upload_view(request):
     swordCollections = swordcnx.parse_service_document(conn.sd)
 
     # Parse form elements
-    import os
     filesToUpload = {}
     for key in ['file1','file2','file3']:
         if hasattr(inputs[key], 'file'):
-            filesToUpload[os.path.basename(inputs[key].filename)] = inputs[key].file
+            filesToUpload[os.path.basename(inputs[key].filename)] = \
+                inputs[key].file
 
     # Send zip file to Connexions through SWORD interface
     conn = swordcnx.Connection(inputs['url'],
-                               user_name=username, user_pass=password,
+                               user_name=username,
+                               user_pass=password,
                                download_service_document=False)
-    response = swordcnx.upload_multipart(
-        conn, inputs['title'], inputs['summary'], inputs['language'],
-        inputs['keywords'].split("\n"), filesToUpload)
+
+    response = swordcnx.upload_multipart(conn,
+                                         inputs['title'],
+                                         inputs['summary'],
+                                         inputs['language'],
+                                         inputs['keywords'].split("\n"),
+                                         filesToUpload)
 
     return {
         'serviceDocument': inputs['serviceDocument'],
