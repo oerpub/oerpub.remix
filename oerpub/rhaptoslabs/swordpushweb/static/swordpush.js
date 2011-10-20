b31ce27d84c55039e76beeb639992757d7025a27
Johan Beyers <jbeyers@juizi.com>
2011-10-20 20:59:01 +0200

    Reorganise and comment javascript

--- oerpub/rhaptoslabs/swordpushweb/static/swordpush.js
+++ oerpub/rhaptoslabs/swordpushweb/static/swordpush.js
@@ -1,20 +1,24 @@
 $(document).ready(function()
 {
 
+    // Slide in the service document picker on the login page.
     $('#pick-sd').click(function(e) {
         e.preventDefault();
         $('#login-line').slideUp('slow');
         $('#sd-picker').slideDown('slow');
     });
 
-    $('#upload-submit').click(function(e) {
-        $('#upload-wait').slideDown('slow');
-    });
-
+    // Return to the default service document url if cancelled.
     $('#sd-cancel').click(function(e) {
         e.preventDefault();
         $('#service_document_url').val($('#service_document_url').attr('default_val'));
         $('#sd-picker').slideUp('slow');
         $('#login-line').slideDown('slow');
     });
+
+    // A friendly message confirming that the upload is happening.
+    $('#upload-submit').click(function(e) {
+        $('#upload-wait').slideDown('slow');
+    });
+
 });
