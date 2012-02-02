3ad3cccc4250d1b4f146bb43ff5bb8a59a0b9b08
Rijk Stofberg <rijk.stofberg@gmail.com>
2012-02-03 00:16:24 +0200

    Get uploads working as part of issue 78

--- oerpub/rhaptoslabs/swordpushweb/static/swordpush-max-01.js
+++ oerpub/rhaptoslabs/swordpushweb/static/swordpush-max-01.js
@@ -337,6 +337,16 @@ $(document).ready(function()
 	$('#edit-new-username').focus();
     });
 
+    // Simple file upload
+    $('input#file-submit').click(function(event){
+        event.preventDefault();
+        $('input#upload').click();
+    });
+    
+    $('input#upload').change(function(event){
+        $('form#uploadform').submit(); 
+    });
+
 });
 
 ///////////////////////////////////
@@ -368,25 +378,24 @@ $(window).resize(function(){
 
 // Google Picker API for the Google Docs import
 function newPicker() {
-google.load('picker', '1', {"callback" : createPicker});
+    google.load('picker', '1', {"callback" : createPicker});
 }       
 
 // Create and render a Picker object for selecting documents
 function createPicker() {
-var picker = new google.picker.PickerBuilder().
-    addView(google.picker.ViewId.DOCUMENTS).
-    setCallback(pickerCallback).
-    build();
-picker.setVisible(true);
+    var picker = new google.picker.PickerBuilder().
+        addView(google.picker.ViewId.DOCUMENTS).
+        setCallback(pickerCallback).
+        build();
+    picker.setVisible(true);
 }
 
 // A simple callback implementation for Picker.
 function pickerCallback(data) {
-if(data.action == google.picker.Action.PICKED){
-    document.getElementById('gdocs_resource_id').value = google.picker.ResourceId.generate(data.docs[0]);
-    document.getElementById('gdocs_access_token').value = data.docs[0].accessToken;
-    $('#file-submit').removeAttr('disabled');
-    $('#file-submit').click();
+    if(data.action == google.picker.Action.PICKED){
+        document.getElementById('gdocs_resource_id').value = google.picker.ResourceId.generate(data.docs[0]);
+        document.getElementById('gdocs_access_token').value = data.docs[0].accessToken;
+        $('#file-submit').removeAttr('disabled');
+        $('#file-submit').click();
+    }
 }
-}
-
