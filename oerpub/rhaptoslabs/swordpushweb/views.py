2928e2ef688c6f0dcd4b8fc32526c449042348e2
Saket Choudhary <saketkc@gmail.com>
2012-05-25 06:22:39 +0100

    New importer.pt(copied template) and upload slides

--- oerpub/rhaptoslabs/swordpushweb/views.py
+++ oerpub/rhaptoslabs/swordpushweb/views.py
@@ -6,6 +6,8 @@ import zipfile
 import traceback
 import libxml2
 import re
+
+
 from cStringIO import StringIO
 from lxml import etree
 from pyramid.view import view_config
@@ -31,7 +33,8 @@ from oerpub.rhaptoslabs.html_gdocs2cnxml.gdocs2cnxml import gdocs_to_cnxml
 import urllib2
 from oerpub.rhaptoslabs.html_gdocs2cnxml.htmlsoup2cnxml import htmlsoup_to_cnxml
 from oerpub.rhaptoslabs.latex2cnxml.latex2cnxml import latex_to_cnxml
-from oerpub.rhaptoslabs.slideimporter.slideshare import main 
+from oerpub.rhaptoslabs.slideimporter.slideshare import upload_to_slideshare
+from oerpub.rhaptoslabs.slideimporter.google_presentations import *
 from utils import escape_system, clean_cnxml, pretty_print_dict, load_config, save_config, add_directory_to_zip
 
 TESTING = False
@@ -1047,5 +1050,23 @@ def importer(request):
     form = Form(request, schema=UploadSchema)
     config = load_config(request)
     field_list = [('upload', 'File')]
+    
+    if form.validate():
+		
+		
+		original_filename = os.path.join(
+                    "/home/saket",
+                    form.data['upload'].filename.replace(os.sep, '_'))
+                saved_file = open(original_filename, 'wb')
+                input_file = form.data['upload'].file
+                shutil.copyfileobj(input_file, saved_file)
+                saved_file.close()
+                input_file.close()
+               
+                respo = upload_to_googledocs("saketkc","howdoyoudothis1314.",original_filename)
+                form.data['upload'] = None
+                
+		
+		return Response(resp)
     response = {'form': FormRenderer(form),'field_list': field_list, 'config': config,}
     return render_to_response(templatePath, response, request=request)
