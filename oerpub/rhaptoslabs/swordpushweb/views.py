2a773a11edca4a1a7b8bfb0f6bed89763823065b
Saket Choudhary <saketkc@gmail.com>
2012-06-04 21:13:32 +0100

    minor fix

--- oerpub/rhaptoslabs/swordpushweb/views.py
+++ oerpub/rhaptoslabs/swordpushweb/views.py
@@ -1047,13 +1047,21 @@ def return_slideshare_upload_form(request):
     check_login(request)
     templatePath = 'templates/importer.pt'
     form = Form(request, schema=UploadSchema)
-    response = {'form':form}
+    response = {'form':FormRenderer(form)}
     validate_form = form.validate()
     if 'form.submitted' in  request.POST:
         if validate_form:
-
-		original_filename = os.path.join(
-                    "/home/saket",
+			## Create a temp directory with the username and current timestamp for it to be unique
+			now_string = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+            # TODO: This has a good chance of being unique, but even so...
+            temp_dir_name = '%s-%s' % (request.session['username'], now_string)
+            save_dir = os.path.join(
+                request.registry.settings['slideshare_import_dir'],
+                temp_dir_name
+                )
+            os.mkdir(save_dir)
+            original_filename = os.path.join(
+                    save_dir,
                     form.data['upload'].filename.replace(os.sep, '_'))
                 saved_file = open(original_filename, 'wb')
                 input_file = form.data['upload'].file
@@ -1061,13 +1069,9 @@ def return_slideshare_upload_form(request):
                 saved_file.close()
                 input_file.close()
                 upload_to_ss = upload_to_slideshare("saketkc",original_filename)
-                print guploader
-                upload_to_gdocs = guploader.upload(original_filename)
-                #guploader.get_resource_id()
-                guploader.get_first_revision_feed()
-                guploader.publish_presentation_on_web()
-                resource_id = guploader.get_resource_id().split(':')[1]
-                form.data['upload'] = None
+                response = {"slideshow_id" : upload_to_ss}
+                templatePath = "templates/slideshare_preview.pt"
+                return render_to_response(templatePath,response,request=request)
 
 
     return render_to_response(templatePath,response,request=request)
