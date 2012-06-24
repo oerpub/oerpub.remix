6a3371d9b85a2ee6230c7dc16de97e13c814b485
gbenga <gbadipe@gmail.com>
2012-06-24 16:32:12 -0500

    UTF-8 fix attempt

--- oerpub/rhaptoslabs/swordpushweb/convert.py
+++ oerpub/rhaptoslabs/swordpushweb/convert.py
@@ -102,7 +102,7 @@ class DocumentConverterClient:
             # Reads and writes converted data to a file
             response = urllib2.urlopen(request).read()
             result_file = open(output_file, 'w')
-            result_file.write(response)            
+            result_file.write(response.encode('utf-8'))           
             t2 = time.time()  
             print 'Conversion Successful! \nConversion took %0.3f ms' % ((t2-t1)*1000.0)
             return True
