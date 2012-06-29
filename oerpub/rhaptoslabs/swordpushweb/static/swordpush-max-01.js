f315f07028f1dcb96fea6664cbf5d9ba6d47d71d
Saket Choudhary <saketkc@gmail.com>
2012-06-29 06:20:48 +0100

    js for choosing import method

--- oerpub/rhaptoslabs/swordpushweb/static/swordpush-max-01.js
+++ oerpub/rhaptoslabs/swordpushweb/static/swordpush-max-01.js
@@ -310,20 +310,21 @@ $(document).ready(function() {
     });
     
     $('input#choose-importer').click(function(event){
-        $("#choose-importer-box").slideUp()
-        $("#file-browser").show().slideDown();
+        $('#choose-importer-box').slideUp()
+        $('#file-browser').show().slideDown();
         if ($('#import-to-google').is(':checked')) {
-            $("input#upload-to-google").val("true");
+            $('input#upload-to-google').val("true");
         
         } 
         if ($('#import-to-ss').is(':checked')) {
-            $("input#upload-to-ss").val("true");
+            $('input#upload-to-ss').val("true");
         
         } 
-        $("input#upload-to-ss").val("true");
-        $("input#upload-to-ss").val("true");
+        $('input#upload-to-ss').val("true");
+        $('input#upload-to-ss').val("true");
         
     });
+    $('#file-browser').hide();
 });
 
 function _doAction(message, event) {
