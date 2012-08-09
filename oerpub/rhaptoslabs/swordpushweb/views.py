62af70291c8b2ef1c7cb73ff2792405bfc67e7dd
Saket Choudhary <saketkc@gmail.com>
2012-08-09 06:28:54 +0530

    iframe test

--- oerpub/rhaptoslabs/swordpushweb/views.py
+++ oerpub/rhaptoslabs/swordpushweb/views.py
@@ -1398,8 +1398,9 @@ def slideshow_preview(request):
         embed_slideshare = True
     templatePath = "templates/google_ss_preview.pt"
     if validate_form:
+        introductory_paragraphs = request.post('introductory_paragraphs')
         j=1
-        cnxml=session["cnxml"]+"""<content><para id="introduction-1">Introduction goes here</para><section id="test-section"><title>Test your knowledge</title>"""
+        cnxml=session["cnxml"]+"""<content><para id="introduction-1">"""+introductory_paragraphs+"""</para><para id="slideshare-embed"><iframe src="http://www.slideshare.net/slideshow/embed_code/"""+slideshare_id+"""" width="425" height="355"></iframe></para>"""+"""<section id="test-section"><title>Test your knowledge</title>"""
         for i in range(1,6):
             form_question = request.POST.get('question-'+str(i))
             print form_question
