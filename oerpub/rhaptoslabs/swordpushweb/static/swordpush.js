d8c03abe48e0b0aecd81bac3269faed26a0e4090
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-09-06 16:41:58 +0200

    jquery to support modules change.

--- oerpub/rhaptoslabs/swordpushweb/static/swordpush.js
+++ oerpub/rhaptoslabs/swordpushweb/static/swordpush.js
@@ -389,7 +389,6 @@ $(document).ready(function()
 
     // Show the chosen workgroup as selected when it's been clicked
     $(".popOut li").has("a").click(function(e){
-      e.preventDefault();
       var popMenu = $(this).closest("li.popMenu");
       element = $(this).find("a");
       $('input#workspace').attr('value', $(element).attr('href'));
@@ -506,7 +505,19 @@ $(document).ready(function()
             $(".forward-button").removeAttr("disabled");
         }
     }); 
-
+    
+    $('.workspace-link').click(function(event) {
+        event.preventDefault();
+        var workspace_url = $(this).attr('href');
+        $.ajax({
+            url: 'modules_list',
+            cache: false,
+            dataType: 'html',
+            data: {'workspace': workspace_url},
+            success: updateModules,
+            error: showError,
+        });
+    });
 });
 
 function _doAction(message, event) {
@@ -740,4 +751,13 @@ function removeFeaturedLink(event) {
     } else {
         return false;
     }
+}
+
+function updateModules(data, textStatus, jqXHR) {
+    html = $(data);
+    $('div#modules-list').replaceWith(html);
+}
+
+function showError(jqXHR, textStatus, errorThrown) {
+    alert(textStatus);
 }
