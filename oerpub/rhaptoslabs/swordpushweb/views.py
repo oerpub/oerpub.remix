e4f9e83f3121b5c30a197126c763852bb94b6b1c
Izak Burger <isburger@gmail.com>
2012-09-14 16:08:52 +0200

    Add a download browser view, so we can later delay actions until delay time.

--- oerpub/rhaptoslabs/swordpushweb/views.py
+++ oerpub/rhaptoslabs/swordpushweb/views.py
@@ -1217,3 +1217,21 @@ def modules_list(request):
                 'config': config,
     }
     return response
+
+@view_config(route_name='download_zip',
+    http_cache=(0, {'no-store': True, 'no-cache': True, 'must-revalidate': True}))
+def download_zip(request):
+    check_login(request)
+
+    res = Response(content_type='application/zip')
+    res.headers.add('Content-Disposition', 'attachment;filename=upload.zip')
+
+    save_dir = os.path.join(request.registry.settings['transform_dir'],
+        request.session['upload_dir'])
+    zipfile = open(os.path.join(save_dir, 'upload.zip'), 'rb')
+    stat = os.fstat(zipfile.fileno())
+    res.app_iter = iter(lambda: zipfile.read(4096), '')
+    res.content_length = stat.st_size
+    res.last_modified = datetime.datetime.utcfromtimestamp(
+        stat.st_mtime).strftime('%a, %d %b %Y %H:%M:%S GMT')
+    return res
