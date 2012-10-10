814aa9d00cc032428985e1aa26a5a6125e3b486a
Saket Choudhary <saketkc@gmail.com>
2012-10-10 15:44:50 +0530

    metadata is not same

--- oerpub/rhaptoslabs/swordpushweb/views.py
+++ oerpub/rhaptoslabs/swordpushweb/views.py
@@ -1527,19 +1527,23 @@ def slideshow_preview(request):
             if form_question:
                 #form_options = request.POST.get('options-'+str(i)).split('\r\n')
                 form_radio_answer = request.POST.get('radio-'+str(i)) #this give us something like 'answer-1-1'. so our solution is this
-                solution = request.POST.get(form_radio_answer)
-                print "FORM RADIO ANSWER",form_radio_answer
-                print "SOLUTION", solution
                 form_all_answers=[]
                 for j in range(1,10):
 					try:
 						form_all_answers[j-1] = request.POST.get('answer-'+str(i)+'-'+str(j))
 					except:
-						break
+						print "No element found"
                 itemlist = ""
-                for item in form_all_answers:
-                    itemlist += "<item>" + item + "</item>"
-                cnxml+="""<exercise id="exercise-"""+str(i)+""""><problem id="problem-"""+str(i)+""""><para id="para-"""+str(i)+"""">"""+str(form_question)+"""<list id="option-list-"""+str(i)+"""" list-type="enumerated" number-style="lower-alpha">"""+str(itemlist)+"""</list></para></problem>"""
+                if form_radio_answer:
+					solution = request.POST.get(form_radio_answer)
+					for item in form_all_answers:
+						itemlist += "<item>" + item + "</item>"
+					cnxml+="""<exercise id="exercise-"""+str(i)+""""><problem id="problem-"""+str(i)+""""><para id="para-"""+str(i)+"""">"""+str(form_question)+"""<list id="option-list-"""+str(i)+"""" list-type="enumerated" number-style="lower-alpha">"""+str(itemlist)+"""</list></para></problem>"""
+				else:
+					solution = request.POST.get('answer-'+str(i)+'-1')
+					cnxml+="""<exercise id="exercise-"""+str(i)+""""><problem id="problem-"""+str(i)+""""><para id="para-"""+str(i)+"""">"""+str(form_question)+"""</para></problem>"""
+                print "FORM RADIO ANSWER",form_radio_answer
+                print "SOLUTION", solution
                 cnxml+=""" <solution id="solution-"""+str(i)+""""> <para id="solution-para-"""+str(i)+"""">"""+solution+"""</para></solution></exercise>"""
 				
 					
