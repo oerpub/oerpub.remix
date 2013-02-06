79f97a1f438102c8a75571e71039ecc934563445
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-02-06 11:33:41 +0200

    New application utility

--- oerpub/rhaptoslabs/swordpushweb/provider.py
+++ oerpub/rhaptoslabs/swordpushweb/provider.py
@@ -18,3 +18,40 @@ class Provider(object):
 
 class WorkflowStepsUtility(object):
     implements(IWorkflowSteps)
+
+    workflows = {'new_module'               : ['', '', ''],
+                 'edit_existing_module'     : [],
+                 'import_new_module'        : [],
+                 'overwrite_existing_module': []}
+   
+    def getWorkflowSteps(self, workflow):
+        steps = self.workflows.get(workflow)
+        if not steps:
+            raise NotFound('Workflow %s could not be found.' % workflow)
+        return steps
+
+    def _getCurrentIdx(self, workflow, step):
+        current_idx = steps.index(current_step)
+        if not current_idx:
+            raise NotFound('Missing workflow step %d.' % current_step)
+        return current_idx
+    
+    def getNextStep(self, workflow, current_step):
+        """
+        We use whatever is less the last index in the steps or the current
+        step +1. That way we don't try to go beyond the end of the steps.
+        """
+        steps = self.getWorkflowSteps(workflow)
+        current_idx = self._getCurrentIdx(workflow,current_step) 
+        next_idx = min(current_idx+1, len(steps)+1)
+        return steps[next_idx]
+
+    def getPreviousStep(self, workflow, current_step):
+        """
+        We use whatever is bigger the first index in the steps or the current
+        step -1. That way we don't try to go beyond the beginning of the steps.
+        """
+        steps = self.getWorkflowSteps(workflow)
+        current_idx = self._getCurrentIdx(workflow,current_step) 
+        next_idx = max(current_idx-1, 0)
+        return steps[next_idx]
