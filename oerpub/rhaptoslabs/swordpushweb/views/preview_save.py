4bf533faaa333587630cfde5abf93dad8419cf28
Izak Burger <isburger@gmail.com>
2013-04-29 23:30:18 +0200

    Get title directly from saved html
    Don't attempt to get it from the transformed html.

--- oerpub/rhaptoslabs/swordpushweb/views/preview_save.py
+++ oerpub/rhaptoslabs/swordpushweb/views/preview_save.py
@@ -5,7 +5,7 @@ from lxml import etree
 from pyramid.view import view_config
 from pyramid.response import Response
 
-from rhaptos.cnxmlutils.utils import aloha_to_etree, html_to_valid_cnxml
+from rhaptos.cnxmlutils.utils import aloha_to_etree, etree_to_valid_cnxml
 
 from choose import validate_cnxml
 from oerpub.rhaptoslabs.swordpushweb.views.utils import (
@@ -28,22 +28,22 @@ def preview_save(request):
 
     conversionerror = ''
 
+    # Get the title from aloha html. We have to do this using a separate
+    # parse operation, because aloha_to_etree below does not give us a
+    # tree on which path() works. A bug or this developer is just stumped.
+    tree = etree.fromstring(html, etree.HTMLParser())
+    try:
+        edited_title = tree.xpath('/html/head/title/text()')[0]
+        request.session['title'] = edited_title
+    except IndexError:
+        if not request.session.get('title', None):
+            request.session['title'] = 'Untitled Document'
+
     #transform preview html to cnxml
     cnxml = None
     try:
         tree = aloha_to_etree(html)           #1 create structured HTML5 tree
-        try:
-            edited_title = tree.xpath('/html/head/title/text()')[0]
-            request.session['title'] = edited_title
-        except IndexError:
-            # If we don't have a title yet, set a default. Most importers
-            # will set one, we don't want to lose it because it isn't
-            # in the html Aloha returns.
-            if not request.session.get('title', None):
-                request.session['title'] = 'Untitled Document'
-
-        cnxml = html_to_valid_cnxml(
-            etree.tostring(tree, pretty_print=True))
+        cnxml = etree_to_valid_cnxml(tree, pretty_print=True)
     except Exception as e:
         #return render_conversionerror(request, str(e))
         conversionerror = str(e)
