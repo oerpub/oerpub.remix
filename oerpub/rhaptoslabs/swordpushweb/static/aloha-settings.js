0fd43e25c0ff0cb6b923e1f8c7d09c244d38bfc7
Izak Burger <isburger@gmail.com>
2013-05-02 16:46:45 +0200

    Get aloha-settings.js in sync with editor.

--- oerpub/rhaptoslabs/swordpushweb/static/aloha-settings.js
+++ oerpub/rhaptoslabs/swordpushweb/static/aloha-settings.js
@@ -13,6 +13,9 @@
         requireConfig: {
             waitSeconds: 42,
             paths: {
+                // Override location of jquery-ui and use our own. Because
+                // jquery-ui and bootstrap conflict in a few cases (buttons,
+                // tooltip) our copy has those removed.
                 jqueryui: '../../oerpub/js/jquery-ui-1.9.0.custom-aloha'
             }
         },
@@ -65,6 +68,11 @@
             oerpub: '../plugins/oerpub',
             oer: '../plugins/oer',
             cnx: '../plugins/cnx'
+        },
+        contentHandler: {
+            insertHtml: [ 'word', 'generic', 'oembed'],
+            initEditable: [],
+            getContents: []
         }
     };
 })(window);
