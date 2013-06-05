a265b223acf7f121177f1d4df106be135f52e58f
Izak Burger <isburger@gmail.com>
2013-06-05 16:25:16 +0200

    Implement anonymous download on restart
    If user chooses to work on a new module, the old saved data is kept
    around a little longer, and a download is automatically initiated from
    within an iframe.

--- oerpub/rhaptoslabs/swordpushweb/views/download_zip.py
+++ oerpub/rhaptoslabs/swordpushweb/views/download_zip.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import shutil
 
 from pyramid.response import Response
 from pyramid.view import view_config
@@ -14,12 +15,30 @@ def download_zip(request):
     res = Response(content_type='application/zip')
     res.headers.add('Content-Disposition', 'attachment;filename=saved-module.zip')
 
+    previous_upload_dir = request.session.get('previous_upload_dir', None)
+    if previous_upload_dir is not None:
+        del(request.session['previous_upload_dir'])
+
+    upload_dir = request.session.get('upload_dir', None) or previous_upload_dir
+    assert upload_dir is not None
+
     save_dir = os.path.join(request.registry.settings['transform_dir'],
-        request.session['upload_dir'])
+        upload_dir)
     zipfile = open(os.path.join(save_dir, 'upload.zip'), 'rb')
     stat = os.fstat(zipfile.fileno())
     res.app_iter = iter(lambda: zipfile.read(4096), '')
     res.content_length = stat.st_size
     res.last_modified = datetime.datetime.utcfromtimestamp(
         stat.st_mtime).strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+    # If there is a previous_upload_dir, kill it. This allows the user
+    # one final chance to download it even if the rest of the session has been
+    # killed. It is okay to delete the upload.zip file, as we still have it
+    # open and unix will delete it on the last close().
+    if previous_upload_dir is not None:
+        p = os.path.join(save_dir, previous_upload_dir)
+        try:
+            shutil.rmtree(save_dir, ignore_errors=True)
+        except OSError:
+            pass
     return res
