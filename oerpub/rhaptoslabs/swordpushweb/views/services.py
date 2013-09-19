9f6cd7666affdcca789a544f2eb75e13f49ba8cb
bnwest <brian.n.west@gmail.com>
2013-09-20 01:04:20 +0200

    add conversion logic

--- oerpub/rhaptoslabs/swordpushweb/views/services.py
+++ oerpub/rhaptoslabs/swordpushweb/views/services.py
@@ -9,38 +9,32 @@ from pyramid.view import view_config
 
 from oerpub.rhaptoslabs.swordpushweb.session import (
     AnonymousSession,)
-from oerpub.rhaptoslabs.swordpushweb.views.utils import (
-    create_save_dir,)
-from utils import (
-    check_login,)
 
-# https://pyramid.readthedocs.org/en/latest/narr/renderers.html#jsonp-renderer
-# pyramid.renderers.JSONP is a JSONP renderer factory helper which implements a hybrid json/jsonp renderer. JSONP is useful for making cross-domain AJAX requests.
+from oerpub.rhaptoslabs.html_gdocs2cnxml.gdocs2cnxml import (
+    gdocs_to_cnxml,)
+
+from oerpub.rhaptoslabs.swordpushweb.views.utils import (
+    create_save_dir, 
+    clean_cnxml, 
+    update_html,)
 
 @view_config(route_name='gdoc2html', renderer="json")
 def gdoc2html(request):
     session = request.session
-    import pdb; pdb.set_trace();
 
     # grab inputs
     if 'html' in request.POST: 
       html = request.POST['html'] 
-    elif 'html' in request.GET: 
-      html = request.GET['html']
     else:
       return
 
     if 'textbook_html' in request.POST:
       textbook_html = request.POST['textbook_html'] is '1'
-    elif 'textbook_html' in request.GET:
-      textbook_html = request.GET['textbook_html'] is '1'
     else:
       textbook_html = True
 
     if 'copy_images' in request.POST:
       copy_images = request.POST['copy_images'] is '1'
-    elif 'copy_images' in request.GET:
-      copy_images = request.GET['copy_images'] is '1'
     else:
       copy_images = False
 
@@ -51,16 +45,24 @@ def gdoc2html(request):
     transform_dir = request.registry.settings['transform_dir']
     user_subdir_name, save_dir = create_save_dir(request)
 
-    # setup response that will be returned
-    #res = Response(content_type='text/html')
-    #res.headers.add('Content-Disposition', 'attachment;filename=gdoc_transform.html')
-    #res.text = html
-    #res.content_length = len(html)
-    # res.last_modified = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-    #res.last_modified = datetime.datetime.utcfromtimestamp(
-    #    datetime.datetime.now()).strftime('%a, %d %b %Y %H:%M:%S GMT')
-    #res.last_modified = datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    # allow cross domain access
     request.response.headers.add('Access-Control-Allow-Origin', '*')
+    
+    # convert gdoc html to cnxml to textbook (aka strcutured) html or aloha-ready html
+    cnxml, objects = gdocs_to_cnxml(html, bDownloadImages=copy_images)
+    cnxml = clean_cnxml(cnxml)
+    title = None
+    metadata = None
+    alohareadyhtml, structuredhtml, conversion_error = update_html(cnxml, title, metadata)
+    if conversion_error is None:
+      if textbook_html:
+        html = structuredhtml
+      else:
+        html = alohareadyhtml
+    else:
+      html = ""
 
-    jsonresult =  { "html": html, "aloha-ready": True }
+    jsonresult =  { "html": html, 
+                    "textbook_html": textbook_html,
+                    "copy_images": copy_images, }
     return jsonresult
